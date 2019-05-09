d71d4216af878aadf4264b8d49682dc054f3c9e4
Alexandre Sablayrolles <asablayrolles@fb.com>
2019-05-09 13:03:45 -0700

    Refactor

--- functions.py
+++ functions.py
@@ -19,34 +19,42 @@ def computemAP(q):
     return map_ / q.shape[0]
 
 
-def getLaplaceIndicator(X, y_bin, idx_tr, model, params):
-    X_tr = X[idx_tr]
+def computeMATT(X, y_bin, idx_public, model, params):
+    X_public = X[idx_public]
 
+    # Training auxiliary regression model on public data
     auxmodel = LogisticRegression(**params)
-    auxmodel.fit(X_tr, 0.5 * (1 + y_bin[idx_tr]))
+    auxmodel.fit(X_public, 0.5 * (1 + y_bin[idx_public]).ravel())
+
+    # Compute gradient of all samples according to this new model
     auxgradients = -expit(-y_bin * np.dot(X, auxmodel.coef_.T))*y_bin*X
 
-    hess_left = expit(np.dot(X_tr, auxmodel.coef_.T)) * X_tr
-    hess_right = expit(-np.dot(X_tr, auxmodel.coef_.T)) * X_tr
-
+    # Compute (and inverse) hessian of auxiliary model
+    hess_left = expit(np.dot(X_public, auxmodel.coef_.T)) * X_public
+    hess_right = expit(-np.dot(X_public, auxmodel.coef_.T)) * X_public
     hess = (params['C'] * np.dot(hess_left.T, hess_right) / hess_right.shape[0]) + np.eye(hess_right.shape[1])
     hess_inverse = np.linalg.inv(hess)
 
-    # \theta_1^* - \theta_0^*
-    diff_theta = - np.dot(auxgradients, hess_inverse) / hess_right.shape[0]
-    indic1 = - 2 * np.dot(auxgradients, model.coef_[0] - auxmodel.coef_[0])
-    indic2 = np.sum(auxgradients * diff_theta, axis=1)
-    # sigmoids = expit(np.dot(X, auxmodel.coef_.T)) * expit(- np.dot(X, auxmodel.coef_.T))
-    # indic_3 = np.sum(np.dot(X, hess_inverse) * X, axis=1) * sigmoids
-    # indic_3 = np.log(1 + indic_3)
+    # Compute MATT
+    matt = - np.dot(auxgradients, model.coef_[0] - auxmodel.coef_[0])
 
-    indic = expit(indic1) #+ indic2
+    return matt
 
-    return indic#indic1, indic2
+def computeMetrics(indicator_train, indicator_test):
+    order = np.argsort(np.concatenate([indicator_train, indicator_test]))
+    gt = np.concatenate([np.ones_like(indicator_train), np.zeros_like(indicator_test)])
+    s = np.cumsum(gt[order])
+    
+    map_train = float(100*computemAP(gt[order][None,::-1]))
+    map_test = float(100*computemAP(1 - gt[order][None,:]))
+    acc = float(100*np.max((np.arange(gt.shape[0]) - 2 * s + s[-1]) / gt.shape[0]))
 
-def getMultiLaplace(X, y_bin, idx_tr, n_tr, p, model, params):
-    indic = np.zeros((X.shape[0]))
-    for _ in range(p):
-        indic += getLaplaceIndicator(X, y_bin, np.random.choice(idx_tr, n_tr, replace=False), model, params)
+    return map_train, map_test, acc
 
-    return indic
+
+#def getMultiLaplace(X, y_bin, idx_public, n_tr, p, model, params):
+#    matt = np.zeros((X.shape[0]))
+#    for _ in range(p):
+#        matt += computeMATT(X, y_bin, np.random.choice(idx_public, n_tr, replace=False), model, params)
+#
+#    return matt
--- logisticregression.py
+++ logisticregression.py
@@ -2,82 +2,91 @@ import numpy as np
 from os.path import join
 from sklearn.linear_model import LogisticRegression
 import argparse
-from functions import computemAP, getLaplaceIndicator, getMultiLaplace
+from functions import computemAP, computeMATT, computeMetrics
 from logger import create_logger
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-C", type=float, default=1e4)
-parser.add_argument("-p", type=int, default=1)
-parser.add_argument("--ntrain", type=int, default=1000)
-parser.add_argument("--seed", type=int, default=0)
-args = parser.parse_args()
 
-state = np.random.RandomState(args.seed)
+def boolify(x):
+    x = x.lower()
+    assert x in ["true", "false"]
 
-root_data = "/private/home/asablayrolles/code/projects/dejalight"
-# X = np.load(join(root_data, "cifar10_gist.npy"))
-X = np.load(join(root_data, "cifar10_cnn.npy"))
-X /= np.linalg.norm(X, axis=1, keepdims=True)
-label = np.load(join(root_data, "cifar10_label.npy")).astype(int)[:,0]
+    return x == "true"
 
-indices = np.nonzero(label >= 8)[0]
-indices = indices[state.permutation(indices.shape[0])]
 
-X = X[indices]
-X = np.concatenate([X, np.ones((X.shape[0], 1))], axis=1)
-y = (label[indices] == 9).astype(int)
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-C", type=float, default=1e4)
+    parser.add_argument("-p", type=int, default=1)
+    parser.add_argument("--ntrain", type=int, default=1000)
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--root_data", type=str, default="/private/home/asablayrolles/code/projects/dejalight")
+    parser.add_argument("--normed", type=boolify, default=True)
 
-params = {
-    'tol': 0.0001,
-    'C': args.C,
-    'fit_intercept': False
-}
+    return parser
 
-model = LogisticRegression(**params)
-logger = create_logger()
 
-ntrain = args.ntrain
-model.fit(X[:ntrain], y[:ntrain])
-print("Train accuracy: %.2f" % model.score(X[:ntrain], y[:ntrain]))
-print("Val accuracy %.2f" % model.score(X[ntrain:2*ntrain], y[ntrain:2*ntrain]))
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+    logger = create_logger()
 
-logger.info("rawlogs: " + str({
-    "train_accuracy": model.score(X[:ntrain], y[:ntrain]),
-    "val_accuracy": model.score(X[ntrain:2*ntrain], y[ntrain:2*ntrain])
-}))
+    state = np.random.RandomState(args.seed)
+    
+    # Loading data
+    X = np.load(join(args.root_data, "cifar10_cnn.npy"))
+    label = np.load(join(args.root_data, "cifar10_label.npy")).astype(int).ravel()
+    if args.normed:
+        X /= np.linalg.norm(X, axis=1, keepdims=True)
+    
+    # Shuffling data
+    indices = np.nonzero(label >= 8)[0]
+    indices = indices[state.permutation(indices.shape[0])]
+    X = X[indices]
+    y = (label[indices] == 9).astype(int)
 
-losses_train = - model.predict_log_proba(X[:ntrain])[np.arange(ntrain), y[:ntrain]]
-losses_test = - model.predict_log_proba(X[ntrain:])[np.arange(X.shape[0] - ntrain), y[ntrain:]]
+    # Add a constant term for bias (simplifies computations for Hessian)
+    X = np.concatenate([X, np.ones((X.shape[0], 1))], axis=1)
+    
+    params = {
+        'tol': 0.0001,
+        'C': args.C,
+        'fit_intercept': False,
+        'solver': 'liblinear',
+    }
+    
+    # Create splits
+    ntrain = args.ntrain
+    train_set  = np.arange(0, ntrain)
+    test_set   = np.arange(ntrain, 2*ntrain)
+    public_set = np.arange(2*ntrain, X.shape[0])
 
-order = np.argsort(np.concatenate([-losses_train, -losses_test[:ntrain]]))
-gt = np.concatenate([np.ones_like(losses_train), np.zeros_like(losses_test[:ntrain])])
-s = np.cumsum(gt[order])
-
-print('MAP (test first): %.2f' % float(100*computemAP(1 - gt[order][None,:])))
-print('MAP (train first): %.2f' % float(100*computemAP(gt[order][None,::-1])))
-print("Best accuracy: %.2f" % float(100*np.max((np.arange(gt.shape[0]) - 2 * s + s[-1]) / gt.shape[0])))
-
-logger.info("rawlogs: " + str({
-    'map_train': float(100*computemAP(gt[order][None,::-1])),
-    'map_test': float(100*computemAP(1 - gt[order][None,:])),
-    'accuracy': float(100*np.max((np.arange(gt.shape[0]) - 2 * s + s[-1]) / gt.shape[0])),
-    'method': 'loss'
-}))
-print(50*"=")
-
-y_bin = 2*(y - 0.5).reshape(-1, 1)
-
-# indic = getLaplaceIndicator(X, y_bin, np.arange(2*ntrain, 3*ntrain), model, params)
-indic = getMultiLaplace(X, y_bin, np.arange(2*ntrain, X.shape[0]), n_tr=ntrain, p=args.p, model=model, params=params)
-log_indic = np.log(indic.max() + 1e-8 - indic)
-
-order = np.argsort(np.concatenate([indic[:ntrain], indic[ntrain:2*ntrain]]))
-gt = np.concatenate([np.ones_like(indic[:ntrain]), np.zeros_like(indic[ntrain:2*ntrain])])
-s = np.cumsum(gt[order])
-
-logger.info("rawlogs: " + str({
-    'map_train': float(100*computemAP(gt[order][None,::-1])),
-    'map_test': float(100*computemAP(1 - gt[order][None,:])),
-    'accuracy': float(100*np.max((np.arange(gt.shape[0]) - 2 * s + s[-1]) / gt.shape[0])),
-    'method': 'hess'
-}))
+    # Fit logistic model
+    model = LogisticRegression(**params)
+    model.fit(X[train_set], y[train_set])
+    logger.info("rawlogs: " + str({
+        "train_accuracy": model.score(X[train_set], y[train_set]),
+        "val_accuracy":   model.score(X[test_set],  y[test_set])
+    }))
+    
+    # MALT uses the loss of each element as indicator of train_set membership
+    losses_train = - model.predict_log_proba(X[train_set])[np.arange(train_set.shape[0]), y[train_set]]
+    losses_test  = - model.predict_log_proba(X[test_set])[np.arange(test_set.shape[0]), y[test_set]]
+    map_train, map_test, acc = computeMetrics(- losses_train, - losses_test)
+    logger.info("rawlogs: " + str({
+        'map_train': map_train,
+        'map_test': map_test,
+        'accuracy': acc,
+        'method': 'malt'
+    }))
+    
+    # MATT use the Hessian to compute train_set membership
+    y_bin = 2*(y - 0.5).reshape(-1, 1)
+    matt = computeMATT(X, y_bin, state.choice(public_set, ntrain, replace=False), model, params)
+    map_train, map_test, acc = computeMetrics(matt[train_set], matt[test_set])
+    logger.info("rawlogs: " + str({
+        'map_train': map_train,
+        'map_test': map_test,
+        'accuracy': acc,
+        'method': 'matt'
+    }))
+    
